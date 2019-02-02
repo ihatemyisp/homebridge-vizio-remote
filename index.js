3312500ca171b26fe77d570ca450704eade20389
Brandon Sellers <32714162+bms2993@users.noreply.github.com>
2019-02-01 21:16:20 -0600

    Added TV remote support for iOS 12.2

--- index.js
+++ index.js
@@ -1,87 +1,141 @@
-var vizio = require('vizio-smart-cast');
-var Accessory, Service, Characteristic;
+var vizio_smart_cast_module = require('vizio-smart-cast');
+var accessory, service, characteristic;
 
-module.exports = function(homebridge) {
-	Service = homebridge.hap.Service;
-	Characteristic = homebridge.hap.Characteristic;
-
-	homebridge.registerAccessory('homebridge-vizio', 'VizioDisplay', VizioDisplay);
+module.exports = function(homebridge)
+{
+        service = homebridge.hap.Service;
+        characteristic = homebridge.hap.Characteristic;
+        homebridge.registerAccessory('homebridge-vizio', 'VizioDisplay', VizioDisplay);
 }
 
-function VizioDisplay(log, config, api) {
+function homebridge_vizio(log, config, api)
+{
+        this.log = log;
+        this.config = config;
+        this.accessories = [];
+        this.name = config.name;
 
-	var platform = this;
-	this.log = log;
-	this.config = config;
-	this.accessories = [];
-	
-	this.log("Connecting to Vizio display…");
+        this.log("Connecting to Device...");
+        this.address = config.address;
+        this.token = config.token;
+        this.device = new vizio_smart_cast_module(this.address);
+        this.device.pairing.useAuthToken(this.token);
 
-	this.deviceAddress = config.address;
-	this.accessToken = config.token;
-	this.displayDevice = new vizio(this.deviceAddress);
-	this.displayDevice.pairing.useAuthToken(this.accessToken);
-	
-	this.log("Connected to Vizio display at " + this.deviceAddress);
+        this.log("Registering Device Service...");
+        var device_service = new service.Television(this.name);
+        device_service
+                .setCharacteristic(characteristic.ConfiguredName, this.name);
+        device_service
+                .setCharacteristic(characteristic.SleepDiscoveryMode, characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE);
+        device_service
+                .getCharacteristic(characteristic.Active)
+                .on('get', this.get_power_state.bind(this))
+                .on('set', this.set_power_state.bind(this));
 
-	//Create a service to expose the power state of the display
-	var powerService = new Service.Switch(this.name);
-	powerService.getCharacteristic(Characteristic.On).on('get', this.getPowerState.bind(this)).on('set', this.setPowerState.bind(this));	
+        this.log("Registering Volume Service...");
+        var volume_service = new service.TelevisionSpeaker(this.name + ' Volume');
+        volume_service
+                .setCharacteristic(characteristic.Active, characteristic.Active.ACTIVE)
+                .setCharacteristic(characteristic.VolumeControlType, characteristic.VolumeControlType.ABSOLUTE);
+        volume_service
+                .getCharacteristic(characteristic.VolumeSelector)
+                .on
+                (
+                        'set', (state, callback) =>
+                        {
+                                this.set_volume_state(state, callback);
+                        }
+                );
+        device_service.addLinkedService(volume_service);
 
-	this.services = [powerService];
-	
+        this.log("Registering Key Service...");
+        device_service
+                .getCharacteristic(characteristic.RemoteKey)
+                .on('set', this.set_key_state.bind(this));
+
+        this.service = [device_service, volume_service];
 }
 
-/* Power Service */
-
-// Get the power state of the display
-VizioDisplay.prototype.getPowerState = function(callback) {
-
-	this.log("Fetching Vizio display power state…");
-
-	if (!this.displayDevice) {
-		this.log("Couldn't fetch Vizio display power state because no display is connected.");
-		callback(null, false);
-		return;
-	}
-	
-	this.displayDevice.power.currentMode().then((result) => {
-		
-		var powerState = result.ITEMS[0].VALUE;
-		var isPowerOn = powerState == 1;
-
-		this.log("Vizio display is", (isPowerOn ? "on" : "off"));
-	
-		callback(null, isPowerOn);
-
-	});
-
+homebridge_vizio.prototype.get_power_state = function(callback)
+{
+        this.log("Getting Device Power State...");
+        this.device.power.currentMode().then
+        (
+                (result) =>
+                {
+                        callback(null, result.ITEMS[0].VALUE);
+                }
+        );
 }
 
-// Set the power state of the display
-VizioDisplay.prototype.setPowerState = function(state, callback) {
-	
-	var powerPromise;
-
-	if (state == 1) {
-		powerPromise = this.displayDevice.control.power.on();
-	}
-	else {
-		powerPromise = this.displayDevice.control.power.off();
-	}
-
-	this.log("Turning Vizio display", (state == 1 ? "on" : "off"));
-
-	powerPromise.then((result) => {
-		
-		var status = result.STATUS.RESULT;
-		var success = status == "SUCCESS" ? 1 : 0;
-		callback(null, success);
-	
-	});
-
+homebridge_vizio.prototype.set_power_state = function(state, callback)
+{
+        this.log("Setting Device Power State...");
+        var power_promise;
+        if (state == 1)
+        {
+                power_promise = this.device.control.power.on();
+        }
+        else
+        {
+                power_promise = this.device.control.power.off();
+        }
+        callback();
 }
 
-VizioDisplay.prototype.getServices = function() {
-	return this.services;
+homebridge_vizio.prototype.set_volume_state = function(state, callback)
+{
+        this.log("Setting Volume State...");
+        var volume_promise;
+        if (state == 0)
+        {
+                volume_promise = this.device.control.volume.up();
+        }
+        else
+        {
+                volume_promise = this.device.control.volume.down();
+        }
+        callback();
+}
+
+homebridge_vizio.prototype.set_key_state = function(state, callback)
+{
+        this.log("Setting key state...");
+        var key_promise;
+        switch (state)
+        {
+                case characteristic.RemoteKey.ARROW_UP:
+                        key_promise = this.device.control.navigate.up();
+                        break;
+                case characteristic.RemoteKey.ARROW_DOWN:
+                        key_promise = this.device.control.navigate.down();
+                        break;
+                case characteristic.RemoteKey.ARROW_LEFT:
+                        key_promise = this.device.control.navigate.left();
+                        break;
+                case characteristic.RemoteKey.ARROW_RIGHT:
+                        key_promise = this.device.control.navigate.right();
+                        break;
+                case characteristic.RemoteKey.SELECT:
+                        key_promise = this.device.control.navigate.ok();
+                        break;
+                case characteristic.RemoteKey.BACK:
+                        key_promise = this.device.control.navigate.back();
+                        break;
+                case characteristic.RemoteKey.EXIT:
+                        key_promise = this.device.control.navigate.exit();
+                        break;
+                case characteristic.RemoteKey.PLAY_PAUSE:
+                        key_promise = this.device.control.media.play();
+                        break;
+                case characteristic.RemoteKey.INFORMATION:
+                        key_promise = this.device.control.info();
+                        break;
+        }
+        callback();
+}
+
+homebridge_vizio.prototype.getServices = function()
+{
+        return this.service;
 }
